e36236b91d9d57c37133b444e60f7e689d29c94e
nacho <nacho.albiol88@gmail.com>
2024-12-19 12:27:45 +0100

    Improve ProjecCard style & Button component

--- src/components/sections/projects/components/ProjectCard.jsx
+++ src/components/sections/projects/components/ProjectCard.jsx
@@ -1,6 +1,8 @@
+import Button from "../../../ui/Button";
+
 export default function ProjectCard({ title, description, image, technologies }) {
   return (
-    <div className="flex flex-col items-center bg-gray-200 dark:bg-cyan-900 border border-sky-800 dark:border-gray-500 rounded-lg shadow-lg md:flex-row">
+    <div className="flex flex-col md:flex-row items-center bg-sky-100/70 dark:bg-cyan-900/70 border border-sky-800 dark:border-teal-600 rounded-lg shadow-lg">
       <img
         className="object-cover w-full rounded-t-lg h-96 md:h-64 md:w-auto md:rounded-none md:rounded-s-lg text-sky-900 dark:text-gray-300"
         src={image}
@@ -8,16 +10,20 @@ export default function ProjectCard({ title, description, image, technologies })
       />
       <div className="flex flex-col justify-between p-4 leading-normal">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-sky-950 dark:text-gray-200">{title}</h5>
-        <p className="mb-3 font-normal text-sky-900 dark:text-gray-300">{description}</p>
-        <ul className="mt-2 flex flex-wrap" aria-label="Technologies used">
+        <ul className="mb-2 flex flex-wrap" aria-label="Technologies used">
           {technologies.map((tech) => (
-            <li key={tech} className="mr-1.5 mt-2">
-              <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+            <li key={tech} className="mr-1.5">
+              <div className="flex items-center rounded-full text-sky-600 dark:text-teal-300 bg-sky-600/10 dark:bg-teal-300/10 px-3 py-1 text-xs font-medium leading-5">
                 {tech}
               </div>
             </li>
           ))}
         </ul>
+        <p className="mb-2 text-sm md:text-lg lg:text-sm text-sky-900 dark:text-gray-300">{description}</p>
+        <div className="flex justify-center gap-4 mt-2">
+          <Button size="small">View Project</Button>
+          <Button size="small">View Code</Button>
+        </div>
       </div>
     </div>
   );
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,14 @@
-export default function Button({ onClick, children, className = "" }) {
+export default function Button({ onClick, children, className = "", size = "default" }) {
+  const sizeClasses = {
+    small: "px-2 py-1 text-xs shadow-[3px_3px_#0f172a] dark:shadow-[3px_3px_#0f172a] hover:shadow-[4px_4px]",
+    default: "px-4 py-2 text-sm shadow-[4px_4px_#0f172a] dark:shadow-[4px_4px_#0f172a] hover:shadow-[6px_6px]",
+  };
+
   return (
     <button
-      className={`px-4 py-2 rounded-lg text-sm tracking-widest font-semibold uppercase text-sky-900 dark:text-gray-200 bg-sky-400 dark:bg-teal-600
-          border-2 border-slate-950 shadow-[4px_4px_#0f172a] dark:shadow-[4px_4px_#0f172a] transition-all duration-300 ease-in-out
-          hover:shadow-[6px_6px] hover:bg-slate-950 dark:hover:bg-slate-950 hover:text-gray-200 hover:shadow-sky-400 dark:hover:shadow-teal-600 ${className}`}
+      className={`${sizeClasses[size]} rounded-lg tracking-widest font-semibold uppercase text-sky-900 dark:text-gray-200
+       bg-sky-400 dark:bg-teal-600 border-2 border-slate-950 transition-all duration-300 ease-in-out 
+       hover:bg-slate-950 dark:hover:bg-slate-950 hover:text-gray-200 hover:shadow-sky-400 dark:hover:shadow-teal-600 ${className}`}
       onClick={onClick}
     >
       {/* Children allow for dynamic content to be passed in, as for example the text of the button */}
