545699469c3360704bc8b32ccb76de561f13a211
nacho <nacho.albiol88@gmail.com>
2024-12-23 15:21:57 +0100

    Add translations and Project data to Projects Section

--- src/components/sections/projects/Projects.jsx
+++ src/components/sections/projects/Projects.jsx
@@ -1,11 +1,13 @@
 import ProjectCard from "./components/ProjectCard";
 import SectionTitle from "../../ui/SectionTitle";
 import { projects } from "../../../constants/projects";
+import { useTranslation } from "react-i18next";
 
 export default function Projects() {
+  const { t } = useTranslation();
   return (
     <section id="projects" className="pt-20 pb-20 p-4 w-full mx-auto max-w-screen-xl">
-      <SectionTitle title="My Projects" />
+      <SectionTitle title={t("projects.title")} />
       <div className="pt-4 pb-4 grid grid-cols-1 lg:grid-cols-2 gap-8">
         {/*Render Project Cards from projects array*/}
         {projects.map((project) => (
